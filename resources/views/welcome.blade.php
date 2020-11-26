6c9e4527f69de8d86ab93dd8eb406fc01c338e33
ValenCardozo <valencardozo7@gmail.com>
2020-11-26 12:02:01 -0300

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+<link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <example-component />
+    </div>
+</body>
+<script src="{{ mix('js/app.js') }}"></script>
